648e748784e49370ac6d882d35237b8f8490b5c1
Torofield <tanino@a2.mbn.or.jp>
2018-05-06 20:43:26 +0900

    add: todo app

---
+++ nuxtjs/todo/pages/todo/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input v-model="newTodo" @keyup.enter="addTodo" >
+    <ul>
+      <li v-for="(todo, index) in todos" :key="todo.id">
+        <span>{{ todo.text }}</span>
+        <button @click="removeTodo(index)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: "",
+      todos: [{ text: "TODOを追加してください" }]
+    }
+  },
+  methods: {
+    addTodo() {
+      var text = this.newTodo.trim()
+      if (text) {
+        this.todos.push({ text: text })
+        this.newTodo = ""
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    }
+  }
+}
+</script>
